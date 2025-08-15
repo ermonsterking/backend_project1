0491b64e5d12231905ac25294aa415f78ad7dc64
ermonsterking <raj8252anshu@gmail.com>
2025-08-15 08:18:58 +0530

    utils code discussion

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express"
+import cors from "cors"
+import cookieParser, { signedCookie } from "cookie-parser"
+const app = express()
+
+
+app.use(cors ({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended: true,limit:"16kb"})) 
+// url ender is when we take data from the url like for example...when we search any thiong on google the space between are get converted into the "+" or "%20"
+app.use(express.static("public")) 
+//  a public cofiguration is set to make for storing public asset like image etc
+
+
+app.use(signedCookie())
+
+// cookie parser is used to access cookis of server from brower basically to performing CRUD OPERATION ON COOKIES
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ dotenv.config({
     path: './env'
 })
 
-connectDB
+connectDB()
+.then( ()  => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at PORT :${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("mongo db connection failed !!!",err);
+})
 
 /*
 
